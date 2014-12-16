84613603d131066967bdc8a1bcc3f7883dd66405
Thomas Steffensen <steffensenthomas@gmail.com>
2014-12-16 14:48:28 +0100

    Matlab files updated

--- NoGUI/stopCentralizedModel.m
+++ NoGUI/stopCentralizedModel.m
@@ -6,8 +6,8 @@ function stopCentralizedModel()
         %Convert log file .mat to .csv
         load('centralizedLog.mat');
 
-        headers = {'sig4:DDS time seconds','sig:4DDS time nanoseconds','sig1:Simulation time','sig1:Turbine Id','sig1:Setpoint','sig3:TurbineId','sig3:Current production','sig3:Max production','sig2:MS since last write'};
-        data = [centralizedLog.signal4.sec.Data centralizedLog.signal4.nanosec.Data centralizedLog.signal1.turbineId.Time centralizedLog.signal1.turbineId.data centralizedLog.signal1.setPoint.data centralizedLog.signal3.turbineId.data centralizedLog.signal3.currentProduction.data centralizedLog.signal3.maxProduction.data centralizedLog.signal2.msSinceLastWrite.data];
+        headers = {'sig4:DDS time seconds','sig:4DDS time nanoseconds','sig2:MS since last write'};
+        data = [centralizedLog.signal2.sec.Data centralizedLog.signal2.nanosec.Data centralizedLog.signal1.msSinceLastWrite.data];
 
         fileBase = 'CentralizedLog';
         fileNumber = 0;
